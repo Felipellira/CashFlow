852f6666300d06444377e7dec28bbbf1bba0d963
Felipellira <felipeluigilira@proton.me>
2025-02-13 19:21:04 -0300

    Refactor test structure and add validation test cases
    Reorganized test project structure for better modularity, relocating files under a unified `tests` directory. Introduced new test cases for `RegisterExpenseValidator` to validate edge scenarios, such as empty titles, invalid dates, and payment types. Added a utility class for generating request objects using the `Bogus` library to streamline test data creation.

---
+++ tests/CommonTestUtilities/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+            .RuleFor(r => r.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(r => r.Amount, f => f.Random.Decimal(1, 10000))
+            .RuleFor(r => r.Date, f => f.Date.Past())
+            .RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>());
+    }
+}
